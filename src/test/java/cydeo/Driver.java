89f7909e88fc70a3ea6580018c44310e60e1c948
A.SMAHI <abderrahim.smahi99@gmail.com>
2022-07-16 09:27:37 -0500

    test2 tag is added

--- src/test/java/cydeo/Driver.java
+++ src/test/java/cydeo/Driver.java
@@ -35,8 +35,8 @@ public class Driver {
                 case "remote-chrome":
                     try {
                         // assign your grid server address
-                        //String gridAddress = "52.90.101.317";
-                        String gridAddress = "54.227.60.27";
+                        String gridAddress = "52.90.101.317";
+                        //String gridAddress = "54.227.60.27";
                         URL url = new URL("http://" + gridAddress + ":4444/wd/hub");
                         DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
                         desiredCapabilities.setBrowserName("chrome");
